23caffb09eed0918bf760a9ebc029151eaeebf8c
Hoor <hoormahmoud000@gmail.com>
2026-09-05 17:53:25 +0300

    update cludinary service

--- lib/core/services/cloudinary_service.dart
+++ lib/core/services/cloudinary_service.dart
@@ -13,37 +13,21 @@ class CloudinaryService {
   });
 
   Future<String?> uploadPhoto() async {
-    try {
-      final XFile? pickedFile = await imagePicker.pickImage(
-        source: ImageSource.gallery,
-      );
+    final XFile? pickedFile = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
 
-      if (pickedFile == null) return null;
+    if (pickedFile == null) return null;
 
-      final File file = File(pickedFile.path);
+    final File file = File(pickedFile.path);
 
-      print('FILE PATH: ${file.path}');
+    final response = await cloudinary.unsignedUpload(
+      file: file.path,
+      fileBytes: await file.readAsBytes(),
+      uploadPreset: 'doctor_hunt_images',
+      resourceType: CloudinaryResourceType.image,
+    );
 
-      final response = await cloudinary.unsignedUpload(
-        file: file.path,
-        fileBytes: await file.readAsBytes(),
-        uploadPreset: 'doctor_hunt_images',
-        resourceType: CloudinaryResourceType.image,
-      );
-
-      print('STATUS CODE: ${response.statusCode}');
-      print('ERROR: ${response.error}');
-      print('URL: ${response.secureUrl}');
-
-      if (response.isSuccessful) {
-        return response.secureUrl;
-      }
-
-      return null;
-    } catch (e, stackTrace) {
-      print('UPLOAD ERROR: $e');
-      print(stackTrace);
-      return null;
-    }
+    return response.secureUrl;
   }
 }
